16c1f48e098c429cded02ad0a7d132993bba0650
AHKV2Developer <oofear@hotmail.com>
2026-05-31 09:27:02 +0100

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'logger-v1';
+const ASSETS = [
+  'public_w.html',
+  'manifest.json',
+  'Logger_Icon_192.png',
+  'Logger_Icon_512.png'
+];
+
+// Install Event - Caches essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); 
+});
+
+// Activate Event - Cleans up old cache versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event - Serves app from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
